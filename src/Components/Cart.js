7f113beb66a49ba521d47406f55a889ddb461239
Nayan-03 <nayanmoradiya127gmail.com>
2024-06-04 22:25:18 -0500

    final

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Cart.css';
 
+
 const Cart = ({ cartItems, setCartItems }) => {
 
     // when user click the finalize purchases button
--- src/Components/ProductData.js
+++ src/Components/ProductData.js
@@ -1,6 +1,8 @@
 import item1 from '../images/item1.jpg';
 import item2 from '../images/item2.webp';
 import item3 from '../images/item3.jpg';
+import item4 from '../images/item4.jpg';
+import item5 from '../images/item5.jpeg';
 
 const ProductData = [
     {
@@ -15,7 +17,7 @@ const ProductData = [
         id: 2,
         Title: 'Frame',
         image: item2, // Imported image
-        price: 200,
+        price: 150,
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo natus facilis aperiam officiis accusantium suscipit sequi pariatur minima eius placeat vitae, commodi quisquam, velit illum quasi similique harum. Quasi, quae.',
         cat: 'frame'
     },
@@ -24,6 +26,24 @@ const ProductData = [
         Title: 'T-shirt',
         image: item3, // Imported image
         price: 300,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo natus facilis aperiam officiis accusantium suscipit sequi pariatur minima eius placeat vitae, commodi quisquam, velit illum quasi similique harum. Quasi, quae.',
+        cat: 't-shirt'
+    },
+
+    {
+        id: 4,
+        Title: 'Camera',
+        image: item4, // Imported image
+        price: 50,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo natus facilis aperiam officiis accusantium suscipit sequi pariatur minima eius placeat vitae, commodi quisquam, velit illum quasi similique harum. Quasi, quae.',
+        cat: 't-shirt'
+    },
+
+    {
+        id: 5,
+        Title: 'Buds',
+        image: item5, // Imported image
+        price: 200,
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo natus facilis aperiam officiis accusantium suscipit sequi pariatur minima eius placeat vitae, commodi quisquam, velit illum quasi similique harum. Quasi, quae.',
         cat: 't-shirt'
     },
